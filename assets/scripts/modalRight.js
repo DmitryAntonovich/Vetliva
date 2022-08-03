b9defe0f57af8a140f80e5aa574b16790db26de8
DmitryAntonovich <ad-it@ck.by>
2022-08-03 17:20:47 +0300

    feat: add header interactive

--- assets/scripts/modalRight.js
+++ assets/scripts/modalRight.js
@@ -11,33 +11,26 @@ $(".right-modal__bg").click(() => {
 });
 
 function modalShow(item) {
-  //   item.css({
-  //     display: "flex",
-  //     visibility: "visible",
-  //     opacity: "1",
-  //     transition: "all 1s",
-  //   });
-
   item.css({
-    transition: "all 0.1s",
     display: "flex",
-  });
-
-  item.animate({
     visibility: "visible",
     opacity: "1",
   });
+
+  // item.animate({
+
+  // });
 }
 
 function modalHide(item) {
-  item.animate({
-    visibility: "none",
-    opacity: "0",
-  });
+  // item.animate({
+  //   visibility: "none",
+  //   opacity: "0",
+  // });
 
   item.css({
     display: "none",
-    // visibility: "none",
-    // opacity: "0",
+    visibility: "none",
+    opacity: "0",
   });
 }
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,3 +11,44 @@ $(function () {
 
   $("#datepicker_to").datepicker("option", $.datepicker.regional["ru"]);
 });
+
+$(".modal-lang").click(function () {
+  $(".modal-lang__options").toggleClass("modal-lang__active");
+  $(".modal-lang").children(".modal-arrow").toggleClass("modal-arrow__active");
+});
+
+$(".modal-currency__current").click(function () {
+  $(".modal-currency__current").toggleClass("modal-lang__select__active");
+  $(".modal-currency__select").toggleClass("modal-currency__select__active");
+});
+
+$(".modal-currency__select__item").click(function () {
+  $(".modal-currency__current").toggleClass("modal-lang__select__active");
+  $(".modal-currency__select").toggleClass("modal-currency__select__active");
+});
+
+$(".modal-lang__current").click(function () {
+  $(".modal-lang-list").toggleClass("modal-lang-list__active");
+});
+
+$(".modal-lang-item").click(function () {
+  $(".modal-lang-list").toggleClass("modal-lang-list__active");
+  $(".modal-lang__current").children("span").html($(this).text());
+});
+
+$(".modal-currency__select__item").click(function () {
+  $(".modal-currency__current").children("span").html($(this).text());
+});
+
+// modal-menu__hide
+$(".modal-menu__rates").click(function () {
+  $(".modal-actions").addClass("modal-menu__hide");
+  $(".modal-actions__rates").addClass("modal-actions__rates__active");
+  $(".modal-nav__back").addClass("modal-nav__back__active");
+});
+
+$(".modal-nav__back").click(function () {
+  $(".modal-actions").removeClass("modal-menu__hide");
+  $(".modal-actions__rates").removeClass("modal-actions__rates__active");
+  $(".modal-nav__back").removeClass("modal-nav__back__active");
+});
