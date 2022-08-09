2009b4458612fa17195a1edb19ac375ee532ec03
RudenokArt <rudenokart@yandex.ru>
2022-08-09 17:32:47 +0300

    blog-page

---
+++ blog.php
@@ -0,0 +1,44 @@
+<?php include_once 'header.php'; ?>
+<link rel="stylesheet" href="assets/pages/blog/blog.css">
+<div class="container blog-wrapper blog_page">
+  <div class="row">
+    <a href="#" class="col-12 blog__nav">
+      <div class="blog__nav__arrow-mobile">
+        <svg width="17" height="10" viewBox="0 0 17 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M16 5.65C16.359 5.65 16.65 5.35899 16.65 5C16.65 4.64101 16.359 4.35 16 4.35V5.65ZM0.54038 4.54038C0.28654 4.79422 0.28654 5.20578 0.54038 5.45962L4.67696 9.59619C4.9308 9.85003 5.34235 9.85003 5.59619 9.59619C5.85003 9.34235 5.85003 8.9308 5.59619 8.67696L1.91924 5L5.59619 1.32304C5.85003 1.0692 5.85003 0.657647 5.59619 0.403806C5.34235 0.149965 4.9308 0.149965 4.67696 0.403806L0.54038 4.54038ZM16 4.35L1 4.35V5.65L16 5.65V4.35Z" fill="#B0B5BD"/>
+        </svg>
+      </div>
+      <div class="blog__nav__arrow-desktop">
+        <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M21 9C21.5523 9 22 8.55228 22 8C22 7.44772 21.5523 7 21 7V9ZM0.292894 7.29289C-0.0976315 7.68342 -0.0976315 8.31658 0.292894 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928932C7.68054 0.538408 7.04738 0.538408 6.65685 0.928932L0.292894 7.29289ZM21 7L1 7V9L21 9V7Z" fill="#B0B5BD"/>
+        </svg>
+      </div>
+      Все статьи
+    </a>
+  </div>
+  <div class="row">
+    <div class="col-lg-9 col-md-12 col-sm-12">
+      <h1 class="blog__title">
+        Путешествуй выгодно с Белкарт!
+      </h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col blog__date">
+      19.08.2022
+    </div>
+  </div>
+  <div class="row justify-content-around">
+    <div class="col-lg-6 col-md-6 col-sm-12 mt-5 mb-5 p-0 blog__image" style="background-image: url(assets/pages/blog/blog-page-image-1.png);"></div>
+    <div class="col-lg-6 col-md-6 col-sm-12 mt-5 mb-5 p-0 blog__image" style="background-image: url(assets/pages/blog/blog-page-image-2.png);"></div>
+  </div>
+  <div class="row">
+    <div class="col-12 blog__text">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta repudiandae non eaque consequuntur libero obcaecati earum nesciunt totam ipsam at enim alias vitae explicabo, dolorum odio adipisci sapiente voluptatem optio?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Consequuntur sequi rerum, inventore ad dignissimos aut aliquam asperiores, eligendi ratione vitae aliquid atque laborum cum quidem, totam fugiat nesciunt porro itaque?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eum ab sequi error temporibus rerum quod perspiciatis asperiores cupiditate sed expedita quaerat ut eos, veritatis officia sit dolore exercitationem nisi.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eum ab sequi error temporibus rerum quod perspiciatis asperiores cupiditate sed expedita quaerat ut eos, veritatis officia sit dolore exercitationem nisi.</p>
+    </div>
+  </div>
+</div>
+<?php include_once 'footer.php'; ?>
