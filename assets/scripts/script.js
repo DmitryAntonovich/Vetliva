cb296ba0a329365029bc8998ca17eaf0d09db674
Антонович Дмитрий <antonovich_d@ck.local>
2022-08-09 18:04:55 +0300

    feat: auth/reg form markdown

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -52,3 +52,76 @@ $(".modal-nav__back").click(function () {
   $(".modal-actions__rates").removeClass("modal-actions__rates__active");
   $(".modal-nav__back").removeClass("modal-nav__back__active");
 });
+
+//password show/hide
+$(".show-pass__wrapper").click(function () {
+  if ($(".reg-form__pass").attr("type") == "password") {
+    $(".show-pass").attr("src", "../../images/passShowed.png");
+    $(".show-pass").addClass("view-pass");
+    $(".reg-form__pass").attr("type", "text");
+  } else {
+    $(".show-pass").attr("src", "../../images/passHide.png");
+    $(".show-pass").removeClass("view-pass");
+    $(".reg-form__pass").attr("type", "password");
+  }
+  return false;
+});
+
+$(".show-pass-confirm__wrapper").click(function () {
+  if ($(".reg-form__pass_confirm").attr("type") == "password") {
+    $(".show-pass-confirm").attr("src", "../../images/passShowed.png");
+    $(".show-pass-confirm").addClass("view-pass");
+    $(".reg-form__pass_confirm").attr("type", "text");
+  } else {
+    $(".show-pass-confirm").attr("src", "../../images/passHide.png");
+    $(".show-pass-confirm").removeClass("view-pass");
+    $(".reg-form__pass_confirm").attr("type", "password");
+  }
+  return false;
+});
+
+// auth/reg controll
+
+$(".auth-login").click(function () {
+  $(".auth-login").toggleClass("auth-register");
+  $(".btn-cross").toggleClass("btn-cross__white");
+
+  $(".auth-form__authorize").toggleClass("auth-form__authorize__hide");
+  $(".auth-form__registration").toggleClass("auth-form__registration__hide");
+
+  $(".auth-form__nav").toggleClass("auth-form__nav__authorize");
+
+  $(".auth-login").text(
+    $(this).text() == "Войти" ? "Зарегистрироваться" : "Войти"
+  );
+
+  if ($(".greetings__text").text() == "Добро пожаловать") {
+    $(".greetings__text").text("Ещё нет аккаунта?");
+    $(".auth-nav__text").text("создайте свой аккаунт, чтобы оставаться с нами");
+  } else {
+    $(".greetings__text").text("Добро пожаловать");
+    $(".auth-nav__text").text(
+      "войдите в свой аккаунт, чтобы оставаться с нами"
+    );
+  }
+});
+
+$(".tab-auth").click(function () {
+  $(".auth-form__authorize").removeClass("auth-form__authorize__hide");
+  $(".auth-form__registration").addClass("auth-form__registration__hide");
+
+  // $(".auth-form__nav").toggleClass("auth-form__nav__authorize");
+
+  $(".auth-tab__active").removeClass("auth-tab__active");
+  $(".tab-auth").addClass("auth-tab__active");
+});
+
+$(".tab-reg").click(function () {
+  $(".auth-form__registration").removeClass("auth-form__registration__hide");
+  $(".auth-form__authorize").addClass("auth-form__authorize__hide");
+
+  // $(".auth-form__nav").toggleClass("auth-form__nav__authorize");
+
+  $(".tab-auth").removeClass("auth-tab__active");
+  $(".tab-reg").addClass("auth-tab__active");
+});
