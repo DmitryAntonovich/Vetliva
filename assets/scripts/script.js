0c4e423c8b2d52729f5487409c5b073ee5ebc46c
DmitryAntonovich <ad-it@ck.by>
2022-08-12 17:45:36 +0300

    fix: some fixed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,106 @@
-$(function () {
-  $("#datepicker_from").datepicker();
-  $("#datepicker_from").datepicker("option", "dateFormat", "dd.mm.yy");
+// $(function () {
+//   $(".datepicker_from").datepicker();
+//   $(".datepicker_from").datepicker("option", "dateFormat", "dd.mm.yy");
 
-  $("#datepicker_from").datepicker("option", $.datepicker.regional["ru"]);
+//   $(".datepicker_from").datepicker("option", $.datepicker.regional["ru"]);
+// });
+
+// $(function () {
+//   $(".datepicker_to").datepicker();
+//   $(".datepicker_to").datepicker("option", "dateFormat", "dd.mm.yy");
+
+//   $(".datepicker_to").datepicker("option", $.datepicker.regional["ru"]);
+// });
+
+$(function () {
+  $.datepicker.regional["ru"] = {
+    closeText: "Закрыть",
+    prevText: "Пред",
+    nextText: "След",
+    currentText: "Сегодня",
+    monthNames: [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ],
+    monthNamesShort: [
+      "Янв",
+      "Фев",
+      "Мар",
+      "Апр",
+      "Май",
+      "Июн",
+      "Июл",
+      "Авг",
+      "Сен",
+      "Окт",
+      "Ноя",
+      "Дек",
+    ],
+    dayNames: [
+      "воскресенье",
+      "понедельник",
+      "вторник",
+      "среда",
+      "четверг",
+      "пятница",
+      "суббота",
+    ],
+    dayNamesShort: ["вск", "пнд", "втр", "срд", "чтв", "птн", "сбт"],
+    dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    weekHeader: "Нед",
+    dateFormat: "dd.mm.yy",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "",
+  };
+  $.datepicker.setDefaults($.datepicker.regional["ru"]);
+
+  var dateFormat = "dd.mm.yy",
+    from = $(".datepicker_from")
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 2,
+        dateFormat: "dd.mm.yy",
+      })
+      .on("change", function () {
+        to.datepicker("option", "minDate", getDate(this));
+      }),
+    to = $(".datepicker_to")
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 2,
+        dateFormat: "dd.mm.yy",
+      })
+      .on("change", function () {
+        from.datepicker("option", "maxDate", getDate(this));
+      });
+
+  function getDate(element) {
+    var date;
+    try {
+      date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+      date = null;
+    }
+
+    return date;
+  }
 });
 
-$(function () {
-  $("#datepicker_to").datepicker();
-  $("#datepicker_to").datepicker("option", "dateFormat", "dd.mm.yy");
-
-  $("#datepicker_to").datepicker("option", $.datepicker.regional["ru"]);
-});
+$(".ui-datepicker-div").after(".booking-form__params");
 
 $(".modal-lang").click(function () {
   $(".modal-lang__options").toggleClass("modal-lang__active");
@@ -53,16 +143,17 @@ $(".modal-nav__back").click(function () {
   $(".modal-nav__back").removeClass("modal-nav__back__active");
 });
 
-<<<<<<< HEAD
+//onclick show options
 $(".nav-booking").click(function () {
   $(".booking-options").toggleClass("booking-options__active");
-  $(".nav-booking").children("svg").toggleClass("arrow-top");
+  $(".nav-booking__svg").toggleClass("nav-booking__svg__active");
 });
 
 $(".tourists__select__count").click(function () {
   $(".tourists-options").toggleClass("tourists-options__active");
-  $(".tourists__select__count").children("svg").toggleClass("arrow-top");
-=======
+  $(".tourists-count__arrow").toggleClass("svg-arrow__active");
+});
+
 //password show/hide
 $(".show-pass__wrapper").click(function () {
   if ($(".reg-form__pass").attr("type") == "password") {
@@ -91,7 +182,6 @@ $(".show-pass-confirm__wrapper").click(function () {
 });
 
 // auth/reg controll
-
 $(".auth-login").click(function () {
   $(".auth-login").toggleClass("auth-register");
   $(".btn-cross").toggleClass("btn-cross__white");
@@ -120,8 +210,6 @@ $(".tab-auth").click(function () {
   $(".auth-form__authorize").removeClass("auth-form__authorize__hide");
   $(".auth-form__registration").addClass("auth-form__registration__hide");
 
-  // $(".auth-form__nav").toggleClass("auth-form__nav__authorize");
-
   $(".auth-tab__active").removeClass("auth-tab__active");
   $(".tab-auth").addClass("auth-tab__active");
 });
@@ -130,9 +218,32 @@ $(".tab-reg").click(function () {
   $(".auth-form__registration").removeClass("auth-form__registration__hide");
   $(".auth-form__authorize").addClass("auth-form__authorize__hide");
 
-  // $(".auth-form__nav").toggleClass("auth-form__nav__authorize");
-
   $(".tab-auth").removeClass("auth-tab__active");
   $(".tab-reg").addClass("auth-tab__active");
->>>>>>> cb296ba0a329365029bc8998ca17eaf0d09db674
 });
+
+//active booking tab
+
+$(".form-content__item").click(function () {
+  $(".tab_active").removeClass("tab_active");
+  $(this).addClass("tab_active");
+});
+
+//text toggle
+
+function windowSizeText() {
+  if ($(window).width() <= "768") {
+    $(".text-desktop").css("display", "none");
+    $(".text-mobile").css("display", "block");
+  } else {
+    $(".text-desktop").css("display", "block");
+    $(".text-mobile").css("display", "none");
+  }
+}
+
+windowSizeText();
+
+$(window).load(windowSizeText); // при загрузке
+$(window).resize(windowSizeText); // при изменении размеров
+// или "два-в-одном", вместо двух последних строк:
+$(window).on("load resize", windowSizeText);
